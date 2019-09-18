d4aa81f6d375174fb5322915addec446e54ef047
yangdeng <yangdeng@sensorsdata.cn>
2019-09-18 20:07:33 +0800

    tdy_sb

---
+++ src/models/index.js
@@ -0,0 +1,10 @@
+const models = require.context('./', false, /\.js$/);
+
+export function registerModels (app) {
+  models.keys().map(key => {
+    if (key.indexOf('index') === -1) {
+      app.model(models(key).default)
+    }
+    return null
+  })
+}
